d198eab309c39f7852eb206dd7191b1166444dec
user <jcardenasya@gmail.com>
2025-03-24 21:24:58 -0600

    Configuracióin de variables de app_module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('typeorm'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
