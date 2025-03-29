600e4315e36664ce6de43992516f592df4532613
Eduardo Cárdenas <jcardenasya@gmail.com>
2025-03-29 09:35:29 -0600

    Revert "Feature/Primeros endpoints de Users"

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeorm from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,7 +15,6 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => config.get('typeorm'),
     }),
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
