c9f778c037463c5ba229aa4560015c9031a47479
MPinillaDavila <mpinillad97@gmail.com>
2025-04-02 14:42:36 -0500

    creacion de logger global

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { loggerGlobal } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new loggerGlobal().use);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
