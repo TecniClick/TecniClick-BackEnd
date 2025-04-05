0facad0c6aec2891b853aa82119d7f7dbe89c44c
user <jcardenasya@gmail.com>
2025-04-04 23:20:07 -0600

    Ajuste en el TypeORMModule de las entidades agregadas en el feature carpetas

--- back/src/reviews/reviews.module.ts
+++ back/src/reviews/reviews.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsRepository } from './reviews.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from 'src/entities/reviews.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review])],
   controllers: [ReviewsController],
   providers: [ReviewsService, ReviewsRepository],
 })
