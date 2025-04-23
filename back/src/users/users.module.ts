5d22688faaa0d5e25519fb26209d8fe7635dfeba
MPinillaDavila <mpinillad97@gmail.com>
2025-04-23 12:42:10 -0500

    creacion de mails para reviews y borrado de usuario

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UsersRepository } from './users.repository';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MailModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersRepository],
