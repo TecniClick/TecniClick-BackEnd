f968e8f759ec3a076e7adf074df36b1ba62699d9
MPinillaDavila <mpinillad97@gmail.com>
2025-04-29 17:04:27 -0500

    envio de correo al crear cuenta con google

--- back/src/Auth/strategies/google.strategy.ts
+++ back/src/Auth/strategies/google.strategy.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { UsersRepository } from '../../users/users.repository';
 import { UserRole } from 'src/enums/UserRole.enum';
+import { MailService } from 'src/mail/mail.service';
+
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private readonly usersRepository: UsersRepository) {
+  constructor(private readonly usersRepository: UsersRepository, private readonly mailService: MailService) {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -44,6 +46,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       if (!user) {
         // Crear nuevo usuario
         user = await this.usersRepository.saveAUserRepository(userData);
+        await this.mailService.sendGoogleWelcomeEmail(user.email, user.name);
       } else if (user.deletedAt) {
         // Reactivar usuario eliminado
         user.deletedAt = null;
